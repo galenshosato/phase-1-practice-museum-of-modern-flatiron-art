fa9156b0715241a49001da07db9a5afe8a099ad0
Galen Sho Sato <galen.sato@gmail.com>
2023-01-17 15:42:30 -0500

    Started Populating the Exhibit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('Write your code here');
+let firstMuseum
+
+fetch('http://localhost:3000/current-exhibits')
+.then(resp => resp.json())
+.then(exhibit => {
+    renderExhibit(exhibit)
+
+
+})
+
+
+function renderExhibit(exhibit) {
+    const exhibitTitle = document.getElementById("exhibit-title")
+    exhibitTitle.textContent = exhibit.title
+
+    const exhibitDescription = document.getElementById('exhibit-description')
+    exhibitDescription.textContent = exhibit.description
+    
+    
+
+}
